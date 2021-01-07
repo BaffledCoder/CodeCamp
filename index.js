3bb1676e1ac0efe0f8fb65df61d3da6514db893c
BaffledCoder <64409503+BaffledCoder@users.noreply.github.com>
2021-01-08 01:25:24 +0530

    add views

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+// use express router
 app.use('/', require('./routes/index.js'));
+// set up view engine
+app.use('view engine', 'ejs');
+app.set('views','./views')
+
 app.listen(port, function(err){
     if ( err){
         console.log(`Error: ${err} Server is not running!!`);
